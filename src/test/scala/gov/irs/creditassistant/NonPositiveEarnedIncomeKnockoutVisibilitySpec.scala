b0c4a4103483c2d4994e566c2c23d087cfbfe6ce
SSP <150390034+sps-irs@users.noreply.github.com>
2026-04-22 14:34:53 -0400

    Revamp income page (#57)

--- src/test/scala/gov/irs/creditassistant/NonPositiveEarnedIncomeKnockoutVisibilitySpec.scala
+++ src/test/scala/gov/irs/creditassistant/NonPositiveEarnedIncomeKnockoutVisibilitySpec.scala
@@ -21,8 +21,8 @@ final class NonPositiveEarnedIncomeKnockoutVisibilitySpec
   it should "be true when wage and self-employment inputs are complete and earned income is not positive" in {
     val graph = newFactGraph()
     graph.set("/jobsIncomeTotal", Dollar(0))
-    graph.set("/selfEmplomentGrossIncome", Dollar(0))
-    graph.set("/selfEmplomentGrossIncomeWorkRelatedExpenses", Dollar(0))
+    graph.set("/selfEmploymentGrossIncome", Dollar(0))
+    graph.set("/selfEmploymentGrossIncomeWorkRelatedExpenses", Dollar(0))
     booleanAt(graph, knockoutPath) shouldBe true
     booleanAt(graph, "/hasEarnedIncome") shouldBe false
   }
@@ -30,8 +30,8 @@ final class NonPositiveEarnedIncomeKnockoutVisibilitySpec
   it should "be false when jobs income alone makes earned income positive" in {
     val graph = newFactGraph()
     graph.set("/jobsIncomeTotal", Dollar(5000))
-    graph.set("/selfEmplomentGrossIncome", Dollar(0))
-    graph.set("/selfEmplomentGrossIncomeWorkRelatedExpenses", Dollar(0))
+    graph.set("/selfEmploymentGrossIncome", Dollar(0))
+    graph.set("/selfEmploymentGrossIncomeWorkRelatedExpenses", Dollar(0))
     booleanAt(graph, knockoutPath) shouldBe false
     booleanAt(graph, "/hasEarnedIncome") shouldBe true
   }
@@ -39,8 +39,8 @@ final class NonPositiveEarnedIncomeKnockoutVisibilitySpec
   it should "be false when net self-employment makes earned income positive" in {
     val graph = newFactGraph()
     graph.set("/jobsIncomeTotal", Dollar(0))
-    graph.set("/selfEmplomentGrossIncome", Dollar(8000))
-    graph.set("/selfEmplomentGrossIncomeWorkRelatedExpenses", Dollar(0))
+    graph.set("/selfEmploymentGrossIncome", Dollar(8000))
+    graph.set("/selfEmploymentGrossIncomeWorkRelatedExpenses", Dollar(0))
     booleanAt(graph, knockoutPath) shouldBe false
     booleanAt(graph, "/hasEarnedIncome") shouldBe true
   }
